3fa3feb3b81185efe43a357b3fdee116c7de4561
yuzouyi <redrumZoe@163.com>
2026-09-08 16:29:34 +0800

    Fix config initialization

--- config/config.go
+++ config/config.go
@@ -1,23 +1,32 @@
 package config
 
+import "github.com/spf13/viper"
+
 type Config struct {
-    App struct {
-        Name string
-        Port string
-    }
-    Database struct {
-        Host     string
-        Port     string
-        User     string
-        Password string
-        Name     string
-    }
+	App struct {
+		Name string
+		Port string
+	}
+	Database struct {
+		Host     string
+		Port     string
+		User     string
+		Password string
+		Name     string
+	}
 }
 
 var AppConfig *Config
 
-func InitConfig() {
-    viper.SerConfigName("config")
-    viper.AddConfigPath("yml")
-    viper.A
+func InitConfig() error {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath("./config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	AppConfig = &Config{}
+	return viper.Unmarshal(AppConfig)
 }
